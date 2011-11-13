8d52d649efb760311883d6984dc2fdc473905c37
mike stedman <github@ravuya.com>
2011-11-12 22:00:33 -0700

    adding blitting and data storage

--- voxelhose.h
+++ voxelhose.h
@@ -1,16 +1,58 @@
 #ifndef _voxelhose_h_
 #define _voxelhose_h_
 
+#include <cassert>
+
 class vxVoxelBuffer {
 public:
-	vxVoxelBuffer(unsigned int width, unsigned int height, unsigned int depth);
-	~vxVoxelBuffer();
+	vxVoxelBuffer(unsigned int width, unsigned int height, unsigned int depth) {
+		this->voxels = new unsigned char[depth * height * width];
+	}
+	~vxVoxelBuffer() {
+		delete[] this->voxels;
+		this->voxels = NULL;
+	}
 public:
-	void set(unsigned char colour, unsigned int x, unsigned int y, unsigned int z);
-	unsigned char get(unsigned int x, unsigned int y, unsigned int z);
+	void set(unsigned char colour, unsigned int x, unsigned int y, unsigned int z) {
+		assert(x < width);
+		assert(y < height);
+		assert(z < depth);
+
+		this->voxels[z][y][x] = colour;
+	}
+	unsigned char get(unsigned int x, unsigned int y, unsigned int z) {
+		assert(x < width);
+		assert(y < height);
+		assert(z < depth);
+
+		return this->voxels[z][y][x];
+	}
 public:
 	/// blit this voxel buffer into another one at the given offset
-	void blit(vxVoxelBuffer& destination, unsigned int destinationOffsetX, unsigned int destinationOffsetY, unsigned int destinationOffsetY) const;
+	void blit(vxVoxelBuffer& destination, int destinationOffsetX, int destinationOffsetY, int destinationOffsetY) const {
+		for(unsigned int x = 0; x < width; x++) {
+			int destinationX = x + destinationOffsetX;
+			if(destinationX < 0 || destinationX >= destination.width) {
+				continue;
+			}
+
+			for(unsigned int y = 0; y < height; y++) {
+				int destinationY = y + destinationOffsetY;
+				if(destinationY < 0 || destinationY >= destination.height) {
+					continue;
+				}
+
+				for(unsigned int z = 0; z < depth; z++) {
+					int destinationZ = z + destinationOffsetZ;
+					if(destinationZ < 0 || destinationZ >= destination.depth) {
+						continue;
+					}
+
+					destination.set(this->get(x, y, z), (unsigned int)destinationX, (unsigned int)destinationY, (unsigned int)destinationZ);
+				}
+			}
+		}
+	}
 private:
 	unsigned int width;
 	unsigned int height;
